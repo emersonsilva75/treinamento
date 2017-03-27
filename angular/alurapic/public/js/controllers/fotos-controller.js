8d042c95ddd477eb188aaed26a0a5e7a0af2e17e
Emerson Rodrigues <emerson.rodsilva@gmail.com>
2017-03-27 18:30:52 -0300

    Mudanca permissao

--- angular/alurapic/public/js/controllers/fotos-controller.js
+++ angular/alurapic/public/js/controllers/fotos-controller.js
@@ -1,36 +1,60 @@
-angular.module('alurapic').controller('FotosController',function($scope,$http){
+angular.module('alurapic').controller('FotosController',function($scope,$http,$resource){
 
- $scope.fotos = [];
- $scope.filtro = '';
- $scope.mensagem = '';
+	$scope.fotos = [];
+	$scope.filtro = '';
+	$scope.mensagem = '';
 
-$http.get('v1/fotos')
-.success(function(fotos){
+	var recursoFoto = $resource('v1/fotos/:fotoId');
+
+	recursoFoto.query(function(fotos){
+		$scope.fotos = fotos;
+	},function(erro){
+		console.log(erro);
+	});
+
+	/* Usando http
+	$http.get('v1/fotos')
+	.success(function(fotos){
 	$scope.fotos = fotos;
 })
 .error(function(erro){
-	console.log(erro);
+console.log(erro);
 })
+*/
 
 $scope.remover = function(foto){
-	$http.delete('v1/fotos/' + foto._id)
-	.success(function(){
+
+	recursoFoto.delete({fotoId: foto._id},function(){
 		var indiceFoto = $scope.fotos.indexOf(foto);
 		$scope.fotos.splice(indiceFoto,1);
 		$scope.mensagem = 'Foto ' + foto.titulo + ' foi removido com sucesso';
-	})
-	.error(function(erro){
+	},function(erro){
 		$scope.mensagem = 'Não foi possivel remover a foto ' + foto.titulo;
-	})
+	});
+
+
+
+	/* Usando http
+	$http.delete('v1/fotos/' + foto._id)
+	.success(function(){
+	var indiceFoto = $scope.fotos.indexOf(foto);
+	$scope.fotos.splice(indiceFoto,1);
+	$scope.mensagem = 'Foto ' + foto.titulo + ' foi removido com sucesso';
+})
+.error(function(erro){
+$scope.mensagem = 'Não foi possivel remover a foto ' + foto.titulo;
+})
+*/
+
 };
 
 /*
- var promise = $http.get('v1/fotos');
- promise.then(function(retorno){
- 	$scope.fotos = retorno.data;
- }).catch(function(error){
- 	console.log(error);
- }); */
+var promise = $http.get('v1/fotos');
+promise.then(function(retorno){
+$scope.fotos = retorno.data;
+}).catch(function(error){
+console.log(error);
+}); */
 
 
 
--- angular/alurapic/public/js/main.js
+++ angular/alurapic/public/js/main.js
@@ -1,6 +1,6 @@
-angular.module('alurapic',['minhasDiretivas','ngAnimate','ngRoute'])
+angular.module('alurapic',['minhasDiretivas','ngAnimate','ngRoute','ngResource'])
 .config(function($routeProvider,$locationProvider){
-	
+
 $locationProvider.html5Mode(true);
 
 	$routeProvider.when('/fotos',{
