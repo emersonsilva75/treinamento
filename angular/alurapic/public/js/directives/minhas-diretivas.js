d87aecc1b1701d6e670b0ef9c470984070c87c69
Emerson Rodrigues <emerson.rodsilva@gmail.com>
2017-03-30 08:19:32 -0300

    Finalizando o curso

--- angular/alurapic/public/js/directives/minhas-diretivas.js
+++ angular/alurapic/public/js/directives/minhas-diretivas.js
@@ -1,32 +1,32 @@
 angular.module('minhasDiretivas',[])
 .directive('meuPainel',function(){
-
-
+	
+	
 	var ddo = {};
-
+	
 	ddo.restrict = 'AE';
 	ddo.scope = {
 		titulo: '@titulo'
 	};
 	ddo.transclude = true;
-
-
-
-ddo.templateUrl = 'js/directives/meu-painel.html';
+	
+	
+	
+	ddo.templateUrl = 'js/directives/meu-painel.html';
 	return ddo;
 })
 .directive('minhaFoto',function(){
-
+	
 	var foto = {};
-
+	
 	foto.restrict = 'AE';
 	foto.scope = {
 		titulo: '@titulo',
 		url: '@url'
 	};
-
-
-foto.templateUrl = 'js/directives/minha-foto.html';
+	
+	
+	foto.templateUrl = 'js/directives/minha-foto.html';
 	return foto;
 })
 .directive('meuBotaoPerigo',function(){
@@ -44,22 +44,34 @@ foto.templateUrl = 'js/directives/minha-foto.html';
 	foco.restrict = "A";
 	/* Com o $on nesse caso não precisamos de um escopo privado
 	foco.scope = {
-		focado: '=' // Preciso de comportamento ambiguo
-	}; */
-	
-	// .link é um watcher , escutador do DOM
-	foco.link = function(scope,element){
+	focado: '=' // Preciso de comportamento ambiguo
+}; */
+
+// .link é um watcher , escutador do DOM
+foco.link = function(scope,element){
 	scope.$on('fotoCadastrada',function(){
 		element[0].focus();
 	});
 	/* Usando o watcher para monitorar elemento
-		scope.$watch('focado',function(){
-			if(scope.focado){
-					element[0].focus();
-					scope.focado = false;
-			}
-		}); */
-	}
-	
-	return foco;
+	scope.$watch('focado',function(){
+	if(scope.focado){
+	element[0].focus();
+	scope.focado = false;
+}
+}); */
+}
+
+return foco;
+}).directive('meusTitulos',function(){
+	var tit = {};
+	tit.restrict = 'E';
+	tit.template = '<ul><li ng-repeat="titulo in titulos">{{titulo}}</li></ul>';
+	tit.controller = function($scope,recursoFoto){
+		recursoFoto.query(function(fotos) {
+			$scope.titulos = fotos.map(function(foto) {
+				return foto.titulo;
+			});
+		});
+	};
+	return tit;
 });
