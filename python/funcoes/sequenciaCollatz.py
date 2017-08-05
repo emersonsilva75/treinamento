f6d70e1423c23d9b0f9e5a1ebd2f08db736ebf5e
Emerson Rodrigues <emerson.rodsilva@gmail.com>
2017-08-04 21:48:56 -0300

    Acertando a iteracao

--- python/funcoes/sequenciaCollatz.py
+++ python/funcoes/sequenciaCollatz.py
@@ -2,7 +2,7 @@
 
 # Sequencia de collatz / Collatz Sequence
 
-# Também chamado de "o mais simples problema matemático impossível"  
+# Também chamado de "o mais simples problema matemático impossí­vel"  
 # Se o numero for par fazemos uma divisao inteira por 2 (numero // 2), se o numero for impar
 # muliplicamos por 3 e somamos o numero 1 -> ((3 x numero) + 1)
 # iteramos com o resultado até chegar no resultado = 1
@@ -13,16 +13,16 @@
 # we repeting the process until the result being equal 1
 
 def collatz(number):
-    if ((number % 2)== 0):
-        number = (number // 2)
-    else:
-        number = ((3 * number) + 1)
-    return number
+    while(number > 1):
+        if ((number % 2)== 0):
+            number = (number // 2)
+        else:
+            number = ((3 * number) + 1)
+        print(number)
 # Processando e tratando erro no processamento
 # Processing treating errors and exception
 try:    
     number = int(input("Entre com um numero para gerar a sequencia de  Collatz : "))
-    while(number > 1):
-        number = collatz(number)
+    collatz(number)
 except ValueError:
     print("Valor inválido para cálculo")
